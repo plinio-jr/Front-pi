63dd9e750860a7536b3a3384ac0766927fdce8c0
plinio-jr <pliniojunior5@gmail.com>
2022-11-27 00:40:39 -0300

    atualizando

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,15 +1,69 @@
 <template>
-  <div>
-    <h1>Login</h1>
-  </div>
+    <div class="page-log-in">
+      <div class="logo" >
+            <img src="@/assets/image3.jpg" />
+        <div class="columns">
+            <div class="column is-4 is-offset-4">
+                <h1 class="title">Login</h1>
+
+
+                <form @submit.prevent="submitForm">
+                    <div class="cabecalho">
+                        <label>Email</label>
+                        <div class="control">
+                            <input type="text" class="input" v-model="username">
+                        </div>
+                    </div>
+
+                    <div class="field">
+                        <label>Senha</label>
+                        <div class="control">
+                            <input type="password" class="input" v-model="password">
+                        </div>
+                    </div>
+                    
+                    <div class="notification is-danger" v-if="errors.length">
+                        <p v-for="error in errors" v-bind:key="error">{{ error }}</p>
+                    </div>
+
+                    <div class="field">
+                        <div class="control">
+                            <button class="button is-dark">Login</button>
+                        </div>
+                    </div>
+
+                    <hr>
+
+                    <router-link to="/cadastro">clique aqui</router-link> para se Cadastrar!
+                </form>
+            </div>
+          </div>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
 
+export default {
+    name: 'LogIn',
+    data() {
+        return {
+            username: '',
+            password: '',
+            errors: []
+        }
+    },
 }
 </script>
 
-<style>
+<style scoped>
+.logo img {
+  width: 100vw;
+  height: 100vw;
+}
+.logo {
+  width: 100vw;
+  height: 10vw;
+}
 
 </style>
