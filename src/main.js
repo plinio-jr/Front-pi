e1a4e6fdd499889d9193288ea3dd8948199e7bd7
plinio-jr <pliniojunior5@gmail.com>
2022-12-05 20:15:18 -0300

    pinia

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import store from './store'
 import router from './router'
 import axios from 'axios'
+import {createPinia} from 'pinia'
+
+const app = createApp(App);
+
+app.use(router);
+app.use(createPinia());
+app.mount("#app")
 
 axios.defaults.baseURL = 'http://127.0.0.1:8000'
 
